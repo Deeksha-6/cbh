11c2a98739ac1a66e7076b2bc1c07c9d9c1bf3dd
Deeksha Sudhakar <deeksha.sudhakar.6@gmail.com>
2023-02-23 17:22:14 -0800

    Refactoring code
    1.HASH_ALGORITHM - Better to const created instead of creating object for string each time the createHash function is called 
    2.refactoring the if check for event which can be combined together 
    3. Removing unnecessary  check for candidate is string or not when JSON stringify is there 
    4. Grouping all constants together 
    5.Removing TRIVIAL_PARTITION_KEY as it can be returned as default
    6. Using Buffer. from as JSON.stringify to avoid serialization

--- dpk.js
+++ dpk.js
@@ -1,28 +1,23 @@
 const crypto = require("crypto");
+const MAX_PARTITION_KEY_LENGTH = 256;
+const HASH_ALGORITHM = "sha3-512";
 
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
+  if (event && event.partitionKey) {
       candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
+   } else {
+      const data =(Buffer.isBuffer(event) || typeof event === "string") ? event : JSON.stringify(event);
+      candidate = crypto.createHash(HASH_ALGORITHM).update(data).digest("hex");
+   }
+  
+   if (typeof candidate !== "string") {
+     candidate = JSON.stringify(candidate);
+   }
+  
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    candidate = crypto.createHash(HASH_ALGORITHM).update(candidate).digest("hex");
   }
-  return candidate;
+  return candidate ||"0";
 };
